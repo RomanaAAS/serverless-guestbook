5a87f597c104ce539b377efed2a207401a965fcd
RomanaAAS <103629025+RomanaAAS@users.noreply.github.com>
2022-06-03 15:55:20 +0200

    Added my API route

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://6c5340fe.eu-gb.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
